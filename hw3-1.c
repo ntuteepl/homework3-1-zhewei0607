6b013a150c6fd196bd798b62ebf5942924ed4448
zhewei0607 <146732595+zhewei0607@users.noreply.github.com>
2023-10-23 22:33:35 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+    int num,i;
+    scanf("%d",&num);
+    if(num==0 || num==1)
+    {
+        printf("NO");
+    }
+    else if(num==2 || num==3)
+    {
+        printf("YES");
+    }
+    else
+    {
+       for(i=2;i<(num-1);i++)
+       {
+           if(num%i==0)
+           {
+               printf("NO");
+               break;
+           }
+           else
+           {
+              printf("YES");
+              break;
+           }
+       }
+    }
+}
 
